0c977494dcda886def19489a4495a4430a32dee6
Ambery Harris <amberyharris.ah@gmail.com>
2022-12-27 22:07:39 +1100

    Daisy UI set up

---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1 px-2 lg:flex-none">
+        <Link href="/"><a className="text-lg font-bold">Ambery Harris</a></Link>
+      </div>
+      <div className="flex justify-end flex-1 px-2">
+        <div className="flex items-stretch">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost rounded-btn">
+              Mini demos
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+            >
+              <li>
+                <a>TicTacToe</a>
+              </li>
+              <li>
+                <a>File storage</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#86efac",
+          secondary: "#fb7185",
+          accent: "#fef08a",
+          neutral: "#f3f4f6",
+          "base-100": "#44403c",
+          info: "#a78bfa",
+          success: "#10b981",
+          warning: "#fb923c",
+          error: "#e11d48",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
